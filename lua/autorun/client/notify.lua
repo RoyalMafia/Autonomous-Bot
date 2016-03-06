0a15bf486390a6fa579d44672bc4eabb7c8cd536
RoyalMafia <benpotten@gmail.com>
2016-03-06 23:41:11 +0000

    v0.02

--- lua/autorun/client/notify.lua
+++ lua/autorun/client/notify.lua
@@ -1,7 +1,16 @@
 local killedBy = {}
+local arrestedBy = {}
 
-net.Receive("AutoBotTableUpdate", function(pl, len)
+net.Receive("AutoBotTableUpdateRDM", function(pl, len)
 	killedBy = net.ReadTable()
+end)
+
+net.Receive("AutoBotTableUpdateRDA", function(pl, len)
+	arrestedBy = net.ReadTable()
+end)
+
+net.Receive("AutoBotWarn", function(pl, len)
+	chat.AddText(Color(240,70,70), "[AutoBot Warn] ", Color(255,255,255), net.ReadString())
 end)
 
 surface.CreateFont( "F1", {font = "DermaLarge",size = 18,weight = 100,blursize = 0,scanlines = 0,})
@@ -10,7 +19,7 @@ surface.CreateFont( "F3", {font = "DermaLarge",size = 20,weight = 100,blursize =
 
 function DrawMenu()
 	local AMenu = vgui.Create( "DFrame" )
-	AMenu:SetSize(300, 340)
+	AMenu:SetSize(300, 95)
 	AMenu:Center()
 	AMenu:MakePopup()
 	AMenu:SetTitle( "" )
@@ -63,12 +72,32 @@ function DrawMenu()
 
 	RDMButton.DoClick = function()
 		AMenu:Close()
-		DrawTable()
+		RDMDrawTable()
+	end
+
+	local RDAButton = vgui.Create("DButton", AMenu)
+	RDAButton:SetPos( 5, 65)
+	RDAButton:SetSize(AMenu:GetWide() - 10, 25)
+	RDAButton:SetText("")
+
+	RDAButton.Paint = function()
+		if(RDAButton:IsHovered()) then
+			draw.RoundedBox(0,0,0, RDAButton:GetWide(), RDAButton:GetTall(), Color(42,126,251,200))
+			draw.SimpleText("RDA Logs", "F1", RDAButton:GetWide()/2, RDAButton:GetTall()/2, Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		else
+			draw.RoundedBox(0,0,0, RDAButton:GetWide(), RDAButton:GetTall(), Color(42,126,251,255))
+			draw.SimpleText("RDA Logs", "F1", RDAButton:GetWide()/2, RDAButton:GetTall()/2, Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		end
+	end
+
+	RDAButton.DoClick = function()
+		AMenu:Close()
+		RDADrawTable()
 	end
 
 end
 
-function DrawTable()
+function RDMDrawTable()
 	local PlayerTable = vgui.Create( "DFrame" )
 	PlayerTable:SetSize( ScrW() / 1.8, ScrH() / 1.6 )
 	PlayerTable:Center()
@@ -112,19 +141,21 @@ function DrawTable()
 	Scroll:SetPos( 5, 35 )
 
 	local PlayerList = vgui.Create( "DListView", Scroll )
-	PlayerList:SetSize(PlayerTable:GetWide() + 7, PlayerTable:GetTall() - 25)
+	PlayerList:SetSize(PlayerTable:GetWide() + 7, PlayerTable:GetTall() - 40)
 	PlayerList:SetMultiSelect( false )
 	PlayerList:AddColumn( "Time" ) PlayerList:AddColumn( "Player" ) PlayerList:AddColumn( "Kills" ) PlayerList:AddColumn( "Warns" ) PlayerList:AddColumn( "Last Victim" ) PlayerList:AddColumn( "Time Left (secs)" )
+
+	PlayerList.Paint = function() end
 
 	for i = 1, #killedBy do
 		PlayerList:AddLine(killedBy[i].Time, killedBy[i].Attacker:Nick(), killedBy[i].Kills, killedBy[i].Warns, killedBy[i].Victim:Nick(), killedBy[i].TimeLeft)
 
 		PlayerList:GetLine(i).Paint = function()
-			draw.RoundedBox( 0, 0, 0, PlayerList:GetWide(), PlayerList:GetTall(), Color(42,126,251,255) )
-		end
-
-		PlayerList.Paint = function()
-			return true
+			if PlayerList:GetLine(i):IsHovered() then
+				draw.RoundedBox( 0, 0, 0, PlayerList:GetWide(), PlayerList:GetTall(), Color(42,126,251,255) )
+			else
+				draw.RoundedBox( 0, 0, 0, PlayerList:GetWide(), PlayerList:GetTall(), Color(42,126,251,230) )
+			end
 		end
 	end
 
@@ -132,16 +163,131 @@ function DrawTable()
 		local ScoreboardMenu = vgui.Create("DMenu", PlayerList)
 		ScoreboardMenu:AddOption("Remove Warns", function() 
 			net.Start("AutoBotRemoveWarns")
+				net.WriteString("RDM")
 				net.WriteInt(PlayerList:GetSelectedLine(), 16)
 			net.SendToServer()
 			PlayerList:RemoveLine(PlayerList:GetSelectedLine())
 		 end)
+
+		if player.GetAll()[PlayerList:GetSelectedLine()]:IsFrozen() then
+			ScoreboardMenu:AddOption("Unfreeze player", function()
+				net.Start("AutoBotFreeze")
+					net.WriteEntity(player.GetAll()[PlayerList:GetSelectedLine()])
+					net.WriteBool(false)
+				net.SendToServer()
+			end)
+		else
+			ScoreboardMenu:AddOption("Freeze player", function()
+				net.Start("AutoBotFreeze")
+					net.WriteEntity(player.GetAll()[PlayerList:GetSelectedLine()])
+					net.WriteBool(true)
+				net.SendToServer()
+			end)
+		end
+	
 		ScoreboardMenu:Open() 
 
 		ScoreboardMenu.Paint = function()
-			draw.RoundedBox( 0, 0, 0, PlayerTable:GetWide(), PlayerTable:GetTall(), Color( 240, 70, 70, 255 ) )
+			draw.RoundedBox( 0, 0, 0, PlayerTable:GetWide(), PlayerTable:GetTall(), Color( 42,126,251,255 ) )
 		end
 	end
+end
+
+function RDADrawTable()
+	local PlayerTable = vgui.Create( "DFrame" )
+	PlayerTable:SetSize( ScrW() / 1.8, ScrH() / 1.6 )
+	PlayerTable:Center()
+	PlayerTable:SetTitle( "" )
+	PlayerTable:SetVisible( true )
+	PlayerTable:SetDraggable( false )
+	PlayerTable:ShowCloseButton( false ) 
+	PlayerTable:MakePopup()
+	PlayerTable.Paint = function()
+
+		draw.RoundedBox( 0, 0, 0, PlayerTable:GetWide(), PlayerTable:GetTall(), Color( 30,30,30, 240 ) )
+		draw.RoundedBox( 0, 0, 0, PlayerTable:GetWide(), 30, Color( 42,126,251,255 ) )
+
+		draw.SimpleText("RDA Logs", "F3", PlayerTable:GetWide()/2, 13, Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+	end
+
+	local DermaCloseButton = vgui.Create("DButton")
+	DermaCloseButton:SetParent( PlayerTable )
+	DermaCloseButton:SetText( "" )
+	DermaCloseButton:SetPos( PlayerTable:GetWide() - 40, 5 )
+	DermaCloseButton:SetSize( 35, 20 )
+
+	DermaCloseButton.Paint = function()
+
+		if(DermaCloseButton:IsHovered()) then
+			draw.RoundedBox(0,0,0, DermaCloseButton:GetWide(), DermaCloseButton:GetTall(), Color(30,30,30,150))
+			draw.SimpleText("X", "F1", DermaCloseButton:GetWide()/2, DermaCloseButton:GetTall()/2, Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		else
+			draw.RoundedBox(0,0,0, DermaCloseButton:GetWide(), DermaCloseButton:GetTall(), Color(30,30,30,100))
+			draw.SimpleText("X", "F1", DermaCloseButton:GetWide()/2, DermaCloseButton:GetTall()/2, Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		end
+
+	end
+
+	DermaCloseButton.DoClick = function ()
+		PlayerTable:Close()
+	end
+
+	local Scroll = vgui.Create( "DScrollPanel", PlayerTable )
+	Scroll:SetSize( PlayerTable:GetWide() - 10, PlayerTable:GetTall() - 40 )
+	Scroll:SetPos( 5, 35 )
+
+	local PlayerList = vgui.Create( "DListView", Scroll )
+	PlayerList:SetSize(PlayerTable:GetWide() + 7, PlayerTable:GetTall() - 40)
+	PlayerList:SetMultiSelect( false )
+	PlayerList:AddColumn( "Time" ) PlayerList:AddColumn( "Player" ) PlayerList:AddColumn( "Arrests" ) PlayerList:AddColumn( "Warns" ) PlayerList:AddColumn( "Arrested" ) PlayerList:AddColumn( "Time Left (secs)" )
+
+	PlayerList.Paint = function() end
+
+	for i = 1, #killedBy do
+		PlayerList:AddLine(arrestedBy[i].Time, arrestedBy[i].Arrestor:Nick(), arrestedBy[i].Arrests, killedBy[i].Warns, arrestedBy[i].Arrested:Nick(), arrestedBy[i].TimeLeft)
+
+		PlayerList:GetLine(i).Paint = function()
+			if PlayerList:GetLine(i):IsHovered() then
+				draw.RoundedBox( 0, 0, 0, PlayerList:GetWide(), PlayerList:GetTall(), Color(42,126,251,255) )
+			else
+				draw.RoundedBox( 0, 0, 0, PlayerList:GetWide(), PlayerList:GetTall(), Color(42,126,251,230) )
+			end
+		end
+	end
+
+	PlayerList.OnRowRightClick = function()
+		local ScoreboardMenu = vgui.Create("DMenu", PlayerList)
+		ScoreboardMenu:AddOption("Remove Warns", function() 
+			net.Start("AutoBotRemoveWarns")
+				net.WriteString("RDA")
+				net.WriteInt(PlayerList:GetSelectedLine(), 16)
+			net.SendToServer()
+			PlayerList:RemoveLine(PlayerList:GetSelectedLine())
+		 end)
+
+		if player.GetAll()[PlayerList:GetSelectedLine()]:IsFrozen() then
+			ScoreboardMenu:AddOption("Unfreeze player", function()
+				net.Start("AutoBotFreeze")
+					net.WriteEntity(player.GetAll()[PlayerList:GetSelectedLine()])
+					net.WriteBool(false)
+				net.SendToServer()
+			end)
+		else
+			ScoreboardMenu:AddOption("Freeze player", function()
+				net.Start("AutoBotFreeze")
+					net.WriteEntity(player.GetAll()[PlayerList:GetSelectedLine()])
+					net.WriteBool(true)
+				net.SendToServer()
+			end)
+		end
+	
+		ScoreboardMenu:Open() 
+
+		ScoreboardMenu.Paint = function()
+			draw.RoundedBox( 0, 0, 0, PlayerTable:GetWide(), PlayerTable:GetTall(), Color( 42,126,251,255 ) )
+		end
+	end
+
 end
 
 hook.Add( "OnPlayerChat", "DrawTableList", function( ply, strText, bTeam, bDead )
